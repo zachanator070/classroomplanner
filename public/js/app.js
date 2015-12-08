fadf45475bd59e2b382e2afb03ba5948054f2c15
Jesse Thomson <jessethomson11@gmail.com>
2015-12-08 15:24:38 -0700

    Dropdown filters now working on late and expired assigmnent viewers

--- public/js/app.js
+++ public/js/app.js
@@ -2318,16 +2318,51 @@ webpackJsonp([1],{
 	var React = __webpack_require__(/*! react */ 1);
 	var ReactRouter = __webpack_require__(/*! react-router */ 159);
 	
+	var api = __webpack_require__(/*! ./api.js */ 214);
+	var auth = __webpack_require__(/*! ./auth.js */ 209);
+	
 	var TabBar = __webpack_require__(/*! ./tab-bar */ 221);
+	var Dropdown = __webpack_require__(/*! ./dropdown.js */ 230);
 	var SortableTable = __webpack_require__(/*! ./sortable-table.js */ 222);
 	
 	var LateAssignments = React.createClass({
-		displayName: 'LateAssignments',
+		displayName: "LateAssignments",
 	
 		getInitialState: function () {
+			// var assignments will be replaced with api call
+			var assignments = [{ title: "Read pg 12, ex 1-10", subject: "English 7", student: "Billy Bob", dueDate: "11/12/15", expDate: "11/22/2015", submissions: '0', done: 'false' }, { title: "Read pg 18, ex 91-100", subject: "Math 7", student: "Billy Bob", dueDate: "11/13/15", expDate: "11/23/2015", submissions: '1', done: 'true' }, { title: "Read pg 98, ex 4-8", subject: "Reading 7", student: "Sally Sue", dueDate: "11/14/15", expDate: "11/24/2015", submissions: '0', done: 'false' }, { title: "Read pg 33, ex 1-5", subject: "English 7", student: "Sally Sue", dueDate: "11/15/15", expDate: "11/25/2015", submissions: '1', done: 'true' }];
 			return {
-				data: [{ id: 3, name: "Billy Bob", class: "B" }, { id: 1, name: "Tina Turner", class: "A" }, { id: 2, name: "Ken Doll", class: "A" }, { id: 4, name: "Mary Joseph", class: "C" }]
+				data: assignments,
+				displayedData: assignments,
+				studentSelected: '--Student--',
+				subjectSelected: '--Subject--'
 			};
+		},
+		filterData: function (studentSelected, subjectSelected) {
+			var filtered = this.state.data;
+			if (subjectSelected !== '--Subject--') {
+				var filtered = filtered.filter(function (item) {
+					return item.subject == subjectSelected;
+				});
+			};
+			if (studentSelected !== '--Student--') {
+				var filtered = filtered.filter(function (item) {
+					return item.student == studentSelected;
+				});
+			};
+			this.setState({
+				displayedData: filtered,
+				studentSelected: studentSelected,
+				subjectSelected: subjectSelected
+			});
+		},
+		handleSubjectDropdown: function (selected) {
+			console.log(selected);
+			this.filterData(this.state.studentSelected, selected);
+		},
+		handleStudentDropdown: function (selected) {
+			console.log(selected);
+			this.filterData(selected, this.state.subjectSelected);
 		},
 	
 		render: function () {
@@ -2336,13 +2371,27 @@ webpackJsonp([1],{
 				tabData: [{ tabName: "Current Assignments", tabLink: "#/studentassignments/current", active: false }, { tabName: "Late Assignments", tabLink: "#/studentassignments/late", active: true }, { tabName: "Expired Assignments", tabLink: "#/studentassignments/expired", active: false }]
 			};
 	
-			var columns = [{ header: "ID", key: "id" }, { header: "NAME", key: "name" }, { header: "CLASS", key: "class" }];
+			var columns = [{ header: "Title", key: "title" }, { header: "Subject", key: "subject" }, { header: "Student", key: "student" }, { header: "Due Date", key: "dueDate" }, { header: "Expiration Date", key: "expDate" }, { header: "Submissions", key: "submissions" }, { header: "Done", key: "done" }];
+	
+			var subjectDropdown = {
+				title: '--Subject--', //What should show up on the button to open/close the dropdown
+				items: [// List of items to show in the dropdown
+				'--Subject--', 'Math 7', 'English 7', 'Reading 7']
+			};
+	
+			var studentDropdown = {
+				title: '--Student--', //What should show up on the button to open/close the dropdown
+				items: [// List of items to show in the dropdown
+				'--Student--', 'Billy Bob', 'Sally Sue']
+			};
 	
 			return React.createElement(
-				'div',
+				"div",
 				null,
 				React.createElement(TabBar, { data: tabs }),
-				React.createElement(SortableTable, { data: this.state.data, columns: columns })
+				React.createElement(Dropdown, { title: subjectDropdown.title, items: subjectDropdown.items, itemSelected: this.handleSubjectDropdown }),
+				React.createElement(Dropdown, { title: studentDropdown.title, items: studentDropdown.items, itemSelected: this.handleStudentDropdown }),
+				React.createElement(SortableTable, { data: this.state.displayedData, columns: columns })
 			);
 		}
 	});
@@ -2360,16 +2409,51 @@ webpackJsonp([1],{
 	var React = __webpack_require__(/*! react */ 1);
 	var ReactRouter = __webpack_require__(/*! react-router */ 159);
 	
+	var api = __webpack_require__(/*! ./api.js */ 214);
+	var auth = __webpack_require__(/*! ./auth.js */ 209);
+	
 	var TabBar = __webpack_require__(/*! ./tab-bar */ 221);
+	var Dropdown = __webpack_require__(/*! ./dropdown.js */ 230);
 	var SortableTable = __webpack_require__(/*! ./sortable-table.js */ 222);
 	
 	var ExpiredAssignments = React.createClass({
-		displayName: 'ExpiredAssignments',
+		displayName: "ExpiredAssignments",
 	
 		getInitialState: function () {
+			// var assignments will be replaced with api call
+			var assignments = [{ title: "Read pg 12, ex 1-10", subject: "English 7", student: "Billy Bob", dueDate: "11/12/15", expDate: "11/22/2015", submissions: '0', done: 'false' }, { title: "Read pg 18, ex 91-100", subject: "Math 7", student: "Billy Bob", dueDate: "11/13/15", expDate: "11/23/2015", submissions: '1', done: 'true' }, { title: "Read pg 98, ex 4-8", subject: "Reading 7", student: "Sally Sue", dueDate: "11/14/15", expDate: "11/24/2015", submissions: '0', done: 'false' }, { title: "Read pg 33, ex 1-5", subject: "English 7", student: "Sally Sue", dueDate: "11/15/15", expDate: "11/25/2015", submissions: '1', done: 'true' }];
 			return {
-				data: [{ id: 3, name: "Billy Bob", class: "B" }, { id: 1, name: "Tina Turner", class: "A" }, { id: 2, name: "Ken Doll", class: "A" }, { id: 4, name: "Mary Joseph", class: "C" }]
+				data: assignments,
+				displayedData: assignments,
+				studentSelected: '--Student--',
+				subjectSelected: '--Subject--'
 			};
+		},
+		filterData: function (studentSelected, subjectSelected) {
+			var filtered = this.state.data;
+			if (subjectSelected !== '--Subject--') {
+				var filtered = filtered.filter(function (item) {
+					return item.subject == subjectSelected;
+				});
+			};
+			if (studentSelected !== '--Student--') {
+				var filtered = filtered.filter(function (item) {
+					return item.student == studentSelected;
+				});
+			};
+			this.setState({
+				displayedData: filtered,
+				studentSelected: studentSelected,
+				subjectSelected: subjectSelected
+			});
+		},
+		handleSubjectDropdown: function (selected) {
+			console.log(selected);
+			this.filterData(this.state.studentSelected, selected);
+		},
+		handleStudentDropdown: function (selected) {
+			console.log(selected);
+			this.filterData(selected, this.state.subjectSelected);
 		},
 	
 		render: function () {
@@ -2378,13 +2462,27 @@ webpackJsonp([1],{
 				tabData: [{ tabName: "Current Assignments", tabLink: "#/studentassignments/current", active: false }, { tabName: "Late Assignments", tabLink: "#/studentassignments/late", active: false }, { tabName: "Expired Assignments", tabLink: "#/studentassignments/expired", active: true }]
 			};
 	
-			var columns = [{ header: "ID", key: "id" }, { header: "NAME", key: "name" }, { header: "CLASS", key: "class" }];
+			var columns = [{ header: "Title", key: "title" }, { header: "Subject", key: "subject" }, { header: "Student", key: "student" }, { header: "Due Date", key: "dueDate" }, { header: "Expiration Date", key: "expDate" }, { header: "Submissions", key: "submissions" }, { header: "Done", key: "done" }];
+	
+			var subjectDropdown = {
+				title: '--Subject--', //What should show up on the button to open/close the dropdown
+				items: [// List of items to show in the dropdown
+				'--Subject--', 'Math 7', 'English 7', 'Reading 7']
+			};
+	
+			var studentDropdown = {
+				title: '--Student--', //What should show up on the button to open/close the dropdown
+				items: [// List of items to show in the dropdown
+				'--Student--', 'Billy Bob', 'Sally Sue']
+			};
 	
 			return React.createElement(
-				'div',
+				"div",
 				null,
 				React.createElement(TabBar, { data: tabs }),
-				React.createElement(SortableTable, { data: this.state.data, columns: columns })
+				React.createElement(Dropdown, { title: subjectDropdown.title, items: subjectDropdown.items, itemSelected: this.handleSubjectDropdown }),
+				React.createElement(Dropdown, { title: studentDropdown.title, items: studentDropdown.items, itemSelected: this.handleStudentDropdown }),
+				React.createElement(SortableTable, { data: this.state.displayedData, columns: columns })
 			);
 		}
 	});
