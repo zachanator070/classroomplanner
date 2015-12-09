7b232ad06275b0a9749a4e8e4429aa9ecfb19bf9
Zach <zach.johnson070@gmail.com>
2015-12-09 15:08:14 -0700

    fixed type of user when registering

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = require('./models/express.js');
 
 // setup mongoose
 var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost/list');
+var db = mongoose.connect('mongodb://localhost/planner');
 
 // models
 var api = require('./models/api.js');
